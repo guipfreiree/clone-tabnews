fd0041a02a93e9a06b6d9c0232c9f287ecd686ce
Gui Freire <139483898+guipfreiree@users.noreply.github.com>
2023-12-22 20:35:36 +0000

    adiciona `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para estudar programação
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Clarice, te amo. Esposa e mãe incrível. Dê uma risadinha de alegria! ❤️</h1>
+  return (
+    <h1>
+      Clarice, te amo. Esposa e mãe incrível. Dê uma risadinha de alegria! ❤️
+    </h1>
+  );
+}
+
+function test2() {
+  console.log("agora vai");
 }
 
 export default Home;
